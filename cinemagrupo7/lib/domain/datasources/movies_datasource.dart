eea6fb6e86206c5257a10eb6bd56aefbbe705e3f
Alfredo Romel C R <013100688k@uandina.edu.pe>
2024-12-11 09:44:24 -0500

    Creacion de Provider
    para cada categoria

--- cinemagrupo7/lib/domain/datasources/movies_datasource.dart
+++ cinemagrupo7/lib/domain/datasources/movies_datasource.dart
@@ -1,7 +1,7 @@
 import 'package:cinemagrupo7/domain/entities/movie.dart';
 
 abstract class MoviesDatasource {
+  Future<List<Movie>> getNowPlaying({int page = 1});
 
-  Future<List<Movie>> getNowPlaying({ int page = 1 });
-
+  Future<List<Movie>> getPopular({int page = 1});
 }
--- cinemagrupo7/lib/infrastructure/datasources/moviedb_datasource.dart
+++ cinemagrupo7/lib/infrastructure/datasources/moviedb_datasource.dart
@@ -5,39 +5,36 @@ import 'package:cinemagrupo7/infrastructure/mappers/movie_mapper.dart';
 import 'package:cinemagrupo7/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
-
-
-
 class MoviedbDatasource extends MoviesDatasource {
-
   final dio = Dio(BaseOptions(
-    baseUrl: 'https://api.themoviedb.org/3',
-    queryParameters: {
-      'api_key': Environment.theMovieDbKey,
-      'language': 'es-MX'
-    }
-  ));
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Environment.theMovieDbKey,
+        'language': 'es-MX'
+      }));
 
-
-  @override
-  Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    
-    final response = await dio.get('/movie/now_playing',
-    queryParameters: {
-      'page': page
-    }
-    );
-    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+  List<Movie> _jesonToMovies(Map<String, dynamic> json) {
+    final movieDBResponse = MovieDbResponse.fromJson(json);
 
     final List<Movie> movies = movieDBResponse.results
-    .where((moviedb) => moviedb.posterPath != 'no-poster' )
-    .map(
-      (moviedb) => MovieMapper.movieDBToEntity(moviedb)
-    ).toList();
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
 
     return movies;
   }
 
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response =
+        await dio.get('/movie/now_playing', queryParameters: {'page': page});
+    return _jesonToMovies(response.data);
+  }
 
-
+  @override
+  Future<List<Movie>> getPopular({int page = 1}) async {
+    final response =
+        await dio.get('/movie/popular', queryParameters: {'page': page});
+    return _jesonToMovies(response.data);
+  }
 }
